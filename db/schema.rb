43854a629d37edf258cdb344584ed613ae678a72
Juli Emmel <justjiffy@gmail.com>
2017-09-01 10:10:04 -0700

    installing react + webpack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828040123) do
+ActiveRecord::Schema.define(version: 20170831161201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170828040123) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "user_type"
     t.string "address"
     t.string "city"
     t.string "zip"
